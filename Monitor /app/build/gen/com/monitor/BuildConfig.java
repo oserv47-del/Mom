d86b0c718da525a5e2bc283890529c136010b361
oserv47-del <oserv47@gmail.com>
2026-04-20 22:24:46 +0530

    Extract Monitor /app/build/gen/com/monitor/BuildConfig.java from Monitor .zip

---
+++ Monitor /app/build/gen/com/monitor/BuildConfig.java
@@ -0,0 +1,10 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.monitor;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+    public final static String APPLICATION_ID = "com.monitor";
+    public final static String BUILD_TYPE = "debug";
+    public final static long VERSION_CODE = 32;
+    public final static String VERSION_NAME = "3.2";
+}
